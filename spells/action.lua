a6b1cec68fc41f73638e8d7509e5d23015d81b76
Beha <shacknetisp@mail.com>
2018-04-23 17:52:00 -0400

    Antigravity.

--- spells/action.lua
+++ spells/action.lua
@@ -1,9 +1,32 @@
-tigris.magic.register_spell("tigris_magic:test", {
-    description = "Test",
-    cost = {mana = 30, hp = 2, breath = 2},
+tigris.magic.register_spell("tigris_magic:lesser_antigravity", {
+    description = "Lesser Antigravity",
+    cost = {mana = 50},
     emblem = "action",
-    color = "#FF0",
+    color = "#303",
     on_use = function(itemstack, player, pointed_thing)
+        tigris.player.effect(player, "tigris_magic:lesser_antigravity", {
+            duration = 5,
+        })
         return true
+    end,
+})
+
+tigris.player.register_effect("tigris_magic:lesser_antigravity", {
+    description = "Lesser Antigravity",
+    status = "tigris_magic_antigravity.png",
+
+    set = function(player, old, new)
+        return {
+            time = os.time(),
+            duration = math.min(10, (old and math.max(0, ((old.time + old.duration) - os.time())) or 0) + new.duration)
+        }
+    end,
+
+    apply = function(player, e)
+        player:set_physics_override({gravity = 0.1})
+    end,
+
+    stop = function(player, e)
+        player:set_physics_override({gravity = 1})
     end,
 })
