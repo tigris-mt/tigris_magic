3d43b77024b81ff2b464cee4d55e4c14a90b067b
Beha <shacknetisp@mail.com>
2018-04-26 14:02:06 -0400

    Move earthly desolation to action.lua

--- spells/action.lua
+++ spells/action.lua
@@ -1,3 +1,29 @@
+tigris.magic.register_spell("tigris_magic:earthly_desolation", {
+    description = "Earthly Desolation",
+    cost = {mana = 30, hp = 3},
+    emblem = "attack",
+    overlay = "tigris_magic_mana_icon.png",
+    color = "#442",
+    on_use = function(itemstack, player, pointed_thing)
+        local search = vector.new(3, 3, 3)
+        local start = vector.subtract(player:getpos(), search)
+        local last = vector.add(player:getpos(), search)
+        for _,pos in ipairs(minetest.find_nodes_in_area(start, last, {"group:stone", "default:gravel"})) do
+                minetest.remove_node(pos)
+        end
+        return true
+    end,
+})
+
+minetest.register_craft({
+    output = "tigris_magic:earthly_desolation",
+    recipe = {
+        {"tigris_magic:earth_essence", "tigris_mobs:fang", "tigris_magic:earth_essence"},
+        {"default:obsidian", "tigris_magic:earth_essence", "default:obsidian"},
+        {"tigris_magic:force_essence", "tigris_magic:force_essence", "tigris_magic:force_essence"},
+    },
+})
+
 tigris.magic.register_spell("tigris_magic:lesser_antigravity", {
     description = "Lesser Antigravity",
     cost = {mana = 50},
--- spells/attack.lua
+++ spells/attack.lua
@@ -35,29 +35,3 @@ minetest.register_craft({
         {"group:stone", "", "tigris_magic:force_essence"},
     },
 })
-
-tigris.magic.register_spell("tigris_magic:earthly_desolation", {
-    description = "Earthly Desolation",
-    cost = {mana = 30, hp = 3},
-    emblem = "attack",
-    overlay = "tigris_magic_mana_icon.png",
-    color = "#442",
-    on_use = function(itemstack, player, pointed_thing)
-        local search = vector.new(3, 3, 3)
-        local start = vector.subtract(player:getpos(), search)
-        local last = vector.add(player:getpos(), search)
-        for _,pos in ipairs(minetest.find_nodes_in_area(start, last, {"group:stone", "default:gravel"})) do
-                minetest.remove_node(pos)
-        end
-        return true
-    end,
-})
-
-minetest.register_craft({
-    output = "tigris_magic:earthly_desolation",
-    recipe = {
-        {"tigris_magic:earth_essence", "tigris_mobs:fang", "tigris_magic:earth_essence"},
-        {"default:obsidian", "tigris_magic:earth_essence", "default:obsidian"},
-        {"tigris_magic:force_essence", "tigris_magic:force_essence", "tigris_magic:force_essence"},
-    },
-})
